b418959c0f3746d7c2f163bbe605869b39ed8af4
Naima <nbensassi@hotmail.com>
2020-01-11 12:13:15 -0500

    add files

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": process.env.DB_PASS,
-    "database": "database_development",
+    "database": process.env.DB_NAME,
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
