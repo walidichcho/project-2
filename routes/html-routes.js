4f993a4f597325bf6cfa07a17e64048a10ec796e
Rubinho Lima <rubinholima@me.com>
2020-01-18 09:35:30 -0500

    Polish the code

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -19,13 +19,27 @@ module.exports = function (app) {
     });
 
 
-    app.get("/pricing", function (req, res) {
-        res.sendFile(path.join(__dirname, "../app/public/pricing.html"));
+    app.get("/payment", function (req, res) {
+        res.sendFile(path.join(__dirname, "../app/public/payment.html"));
     });
+
+    app.get("/booking", function (req, res) {
+        res.sendFile(path.join(__dirname, "../app/public/booking.html"));
+    });
+
+    app.get("/safety", function (req, res) {
+        res.sendFile(path.join(__dirname, "../app/public/safety.html"));
+    });
+
+    app.get("/faq", function (req, res) {
+        res.sendFile(path.join(__dirname, "../app/public/safety.html"));
+    });
+
+
     // add route loads the add.html page,
     // where users can enter new characters to the db
     app.get("/add", function (req, res) {
-        res.sendFile(path.join(__dirname, "../app/public/contact.html"));
+        res.sendFile(path.join(__dirname, "../app/public/contact-us.html"));
     });
 
     // all route loads the all.html page,
@@ -34,8 +48,8 @@ module.exports = function (app) {
         res.sendFile(path.join(__dirname, "../app/public/all.html"));
     });
 
-    app.get("/pricing", function (req, res) {
-        res.sendFile(path.join(__dirname, "../app/public/pricing.html"));
+    app.get("/about", function (req, res) {
+        res.sendFile(path.join(__dirname, "../app/public/about.html"));
     });
 
     app.get("/signup", function (req, res) {
